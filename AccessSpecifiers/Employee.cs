20a59e7eba90981e5e3a78ef97f9c41a69fc1fde
Deepak Dash <ddash@lutron.com>
2020-09-14 17:02:37 +0530

    Members Accessed with two ways 1.Inheritance 2.Instance of class

---
+++ AccessSpecifiers/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessSpecifiers
+{
+    class Employee
+    {
+        public void PublicMethod()
+        {
+            Console.WriteLine("PublicMethod");
+        }
+        private void PrivateMethod()
+        {
+            Console.WriteLine("PrivateMethod");
+        }
+        protected void ProtectedMethod()
+        {
+            Console.WriteLine("ProtectedMethod");
+        }
+        internal void InternalMethod()
+        {
+            Console.WriteLine("InternalMethod");
+        }
+        protected internal void protectedInternalMethod()
+        {
+            Console.WriteLine("protectedInternalMethod");
+        }
+    }
+}
---
+++ AccessSpecifiers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AccessSpecifiers
+{
+    //These are special kind of modifiers which is used to define the scope of a type and it's member
+    // Scope means who can access or who can't access
+    // private/internal/protected/protected internal/public
+    //Within the class there won't be any restriction only the restriction starts when we access the members of class 
+    //from outside.
+    class Program : Employee
+    {
+        static void Main(string[] args)
+        {
+            //Private method is not accessible through Inheritance
+            // Protected is accessible through inheritance to the child class program.
+            //public method is accessible from Inheritance
+            //Internal is accessible through Inheritance from same project
+            Program p = new Program();
+            p.InternalMethod();
+            p.protectedInternalMethod();
+            p.ProtectedMethod();
+            p.PublicMethod();
+
+            // Private is not accessible in any way through Instance of class
+            // Protected is not accessible through Instance of class
+            //public method is accessible through instance of class
+            //Internal is accessible through Instance of class from same project
+            Employee employee = new Employee();
+            employee.InternalMethod();
+            employee.protectedInternalMethod();
+            employee.PublicMethod();
+        }
+    }
+}
