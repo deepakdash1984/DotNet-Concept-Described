e4a19c2eec0412e6811498bfae875a816c3b7577
Deepak Dash <ddash@lutron.com>
2020-09-14 15:44:46 +0530

    Indexer Explained

---
+++ Indexer/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexer
+{
+    class Employee
+    {
+        
+        //These are the private variables of a class
+        int EmployeeNumber;
+        string FirstName;
+        string LastName;
+
+        //This is a parameterised constructor
+        public Employee(int empNo, string fName, string lName)
+        {
+            this.EmployeeNumber = empNo;
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+        // By using the this keyword on the method the class behaves as a virtual array
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0 )
+                {
+                    return EmployeeNumber;
+                }
+                else if(index ==1)
+                {
+                    return FirstName;
+                }
+                else if (index ==2)
+                {
+                    return LastName;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ Indexer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(1001,"Suresh","Nath"); //creating the Instance of Employee Class
+
+            //Accessing the private variable of a class through Inexer of a class
+            // Private variable of a class can be accessed in 3 ways making variable as public or 
+            //making a get/set accessor for variables or indexer.
+            Console.WriteLine("EmployeeNumber:" +employee[0]);
+            Console.WriteLine("FirstName:" + employee[1]);
+            Console.WriteLine("LastName:" + employee[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
